3d1ea90308993f5970111d417fc328e225ab4031
JBrink8 <jbrinkm@umich.edu>
2023-06-12 11:22:19 -0400

    landing.html -> index.html

---
+++ js/script.js
@@ -0,0 +1,61 @@
+const ZEUStile = document.getElementById('ZEUS-tile');
+const SPARKtile = document.getElementById('SPARK-tile');
+const MSCGtile = document.getElementById('MSCG-tile');
+const PTtile = document.getElementById('PT-tile');
+const SpatDAPytile = document.getElementById('SpatDAPy-tile');
+const CHNtile = document.getElementById('CHN-tile');
+
+const ZEUSdiscript = document.getElementById('ZEUS-discript');
+const SPARKdiscript = document.getElementById('SPARK-discript');
+const MSCGdiscript = document.getElementById('MSCG-discript');
+const PTdiscript = document.getElementById('PT-discript');
+const SpatDAPydiscript = document.getElementById('SpatDAPy-discript');
+const CHNdiscript = document.getElementById('CHN-discript');
+
+const tile_index = document.getElementById('tile_index');
+
+function displayZEUS() {
+    tile_index.style.display = 'none';
+    ZEUSdiscript.style.display = 'block';
+}
+
+function displaySPARK() {
+    tile_index.style.display = 'none';
+    SPARKdiscript.style.display = 'block';
+}
+
+function displayMSCG() {
+    tile_index.style.display = 'none';
+    MSCGdiscript.style.display = 'block';
+}
+
+function displayPT() {
+    tile_index.style.display = 'none';
+    PTdiscript.style.display = 'block';
+}
+
+function displaySpatDAPy() {
+    tile_index.style.display = 'none';
+    SpatDAPydiscript.style.display = 'block';
+}
+
+function displayCHN() {
+    tile_index.style.display = 'none';
+    CHNdiscript.style.display = 'block';
+}
+
+ZEUStile.addEventListener('click', displayZEUS);
+SPARKtile.addEventListener('click', displaySPARK);
+MSCGtile.addEventListener('click', displayMSCG);
+PTtile.addEventListener('click', displayPT);
+SpatDAPytile.addEventListener('click', displaySpatDAPy);
+CHNtile.addEventListener('click', displayCHN)
+
+var exitbuttons = document.getElementsByClassName('exit');
+
+for (let i = 0; i < exitbuttons.length; ++i) {
+    exitbuttons[i].addEventListener('click', function() { 
+        window.location.reload();
+
+    })
+};
