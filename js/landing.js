8ff1b008a865c6e0eed3182fa18d7139dd02e1e3
JBrink8 <jbrinkm@umich.edu>
2023-06-12 11:13:42 -0400

    experience page worked on

--- js/landing.js
+++
@@ -1,7 +0,0 @@
-const ZEUStile = document.getElementById('ZEUS-tile');
-const SPARKtile = document.getElementById('SPARK-tile');
-const MSCGtile = document.getElementById('MSCG-tile');
-const PTtile = document.getElementById('PT-tile');
-const SpatDAPytile = document.getElementById('SpatDAPy-tile');
-const CHNtile = document.getElementById('CHN-tile');
-
