ddaf0ef97179f0e668191373e0f1635c57dcc85a
JBrink8 <jbrinkm@umich.edu>
2023-07-28 22:33:59 -0400

    terminal save to history basic working, need to outline commands and get basic help, clear, ls, and cd functions cmnds working

--- js/alt_js.js
+++ js/alt_js.js
@@ -1,13 +1,16 @@
-const input = document.getElementById('command-input');
+const input = document.getElementById('command-in');
 const history = document.getElementById('history');
+const loc = document.getElementById('location');
 
 function init_listeners() {
     input.addEventListener('keypress', function(event) {
         if (event.key === "Enter") {
             event.preventDefault();
-            var cmd = input.value.split('$')[0];
-            history.innerHTML += '<p>' + input.value + '</p>';
+            //const former textdocument.createElement('span');
+            history.innerHTML += '<p>' + loc.innerText + ' ' + input.value + '</p>';
             input.value = '';
         }
     })
 }
+
+init_listeners();
